2f1e93896f9dc88bcdc1de36f942f00040553ee9
krzysztof.zylka <krzysztof.zylka@wfirma.pl>
2023-03-17 22:49:35 +0100

    #6 new conditions

--- example/condition.php
+++ example/condition.php
@@ -1,26 +1,29 @@
 <?php
 
+use krzysztofzylka\DatabaseManager\Condition;
+use krzysztofzylka\DatabaseManager\Helper\Where;
+
 require('_init.php');
 
 echo '<h1>Condition</h1>';
 echo '<pre>';
 print_r([
-    (string)new \krzysztofzylka\DatabaseManager\Condition('table.column', '=', 'c'),
-    (string)new \krzysztofzylka\DatabaseManager\Condition('test', '!=', '"c"'),
-    (string)new \krzysztofzylka\DatabaseManager\Condition('test', '<>', "'c'"),
-    (string)new \krzysztofzylka\DatabaseManager\Condition('test', 'IN', ['a', 'b', 'c'])
+    (string)new Condition('table.column', '=', 'c'),
+    (string)new Condition('test', '!=', '"c"'),
+    (string)new Condition('test', '<>', "'c'"),
+    (string)new Condition('test', 'IN', ['a', 'b', 'c'])
 ]);
 echo '</pre>';
 
 echo '<h1>Where test</h1>';
 echo '<pre>';
-print_r((new \krzysztofzylka\DatabaseManager\Helper\Where())->getPrepareConditions([
+print_r((new Where())->getPrepareConditions([
     'test' => 'value',
     'OR' => [
         'test2' => 'value2',
         'test3' => 'value3'
     ],
-    new \krzysztofzylka\DatabaseManager\Condition('test4', 'IN', ['value4_1', 'value4_2', 'value4_3'])
+    new Condition('test4', 'IN', ['value4_1', 'value4_2', 'value4_3'])
 ]));
 echo '</pre>';
 
