0f81f464201326977f99acfe633bce4dc7751002
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2024-02-12 17:41:00 +0100

    Add comment

--- src/Cache.php
+++ src/Cache.php
@@ -11,11 +11,22 @@ class Cache
      */
     private static array $data = [];
 
+    /**
+     * Get data from cache
+     * @param string $name
+     * @return mixed
+     */
     public static function getData(string $name): mixed
     {
         return self::$data[$name] ?? null;
     }
 
+    /**
+     * Save data to cache
+     * @param string $name
+     * @param mixed $value
+     * @return void
+     */
     public static function saveData(string $name, mixed $value): void
     {
         self::$data[$name] = $value;
